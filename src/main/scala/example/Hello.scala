737091eb60c7517b13921b4af18dceef56ae55d7
Micha Rutkowski <mrutkowski@globeandmail.com>
2022-06-19 20:04:59 +0200

    IO version

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,5 +1,8 @@
 package example
 
+import cats.Monad
+import cats.effect.{ExitCode, IO, IOApp, Sync}
+import cats.syntax.all._
 import example.Game._
 
 import scala.annotation.tailrec
@@ -39,49 +42,123 @@ object Game {
   }
 }
 
-object GameApp extends App {
+object GameIO {
+  def showPrompt = IO.delay(print("\n(h)eads, (t)ails, or (q)uit: "))
+
+  def getUserInput = IO.delay(StdIn.readLine().trim.toLowerCase)
+
+  def printableFlipResult(flip: String) = IO.pure(flip match {
+    case "h" => "Heads"
+    case "t" => "Tails"
+  })
+
+  def printGame(printableResult: String, state: GameState) = {
+    IO.delay(println(s"Flip was: ${printableResult}")) *> printGameState(state)
+  }
+
+  def printGameOver() = {
+    IO.delay(println("Game Over"))
+  }
+
+  def printGameState(state: GameState) = {
+    IO.delay(println(s"Flips : ${state.flipsCount}, correct: ${state.correctGuesses}"))
+  }
+
+  def calcualteState(tossResult: String, input: String, state: GameState) = {
+    val newState = if (tossResult == input) {
+      state.copy(flipsCount = state.flipsCount + 1, correctGuesses = state.correctGuesses + 1)
+    }
+    else {
+      state.copy(flipsCount = state.flipsCount + 1)
+    }
+    IO.pure(newState)
+  }
+
+  def tossCoin(r: Random) = {
+    IO.pure(r.nextInt(2) match {
+      case 0 => "h"
+      case 1 => "t"
+    })
+  }
+}
+
+trait Console[F[_]] {
+  def readStr: F[String]
+
+  def readBigDecimal: F[BigDecimal]
+
+  def putStrLn(str: String): F[Unit]
+}
+
+object Console {
+
+  def apply[F[_] : Sync]: Console[F] =
+    new Console[F] {
+
+      def readStr: F[String] = Sync[F].delay(StdIn.readLine())
+
+      def readBigDecimal: F[BigDecimal] =
+        readStr.flatMap { str =>
+          try BigDecimal(str).pure[F]
+          catch {
+            case _: Throwable => putStrLn(s"$str is not a BigDecimal") *> readBigDecimal
+            //            case _: Throwable =>
+            //              putStrLn(s"$str is not a BigDecimal")
+            //                .flatMap(_ => readBigDecimal)
+          }
+        }
+
+      def putStrLn(str: String): F[Unit] = Sync[F].delay(println(str))
+    }
+}
+
+
+object GameApp extends IOApp {
   var input = ""
   val state: GameState = GameState(0, 0)
   val r = new Random()
 
-  @tailrec
-  def gameLoop(r: Random, state: GameState): Unit = {
-    showPrompt
-    input = getUserInput
-    if (input == "q") {
-      printGameOver()
-    }
-    else {
-      val tossResult = tossCoin(r)
-      val newState = if (tossResult == input) {
-        state.copy(flipsCount = state.flipsCount + 1, correctGuesses = state.correctGuesses + 1)
-      }
-      else {
-        state.copy(flipsCount = state.flipsCount + 1)
-      }
-      val printableFlipResultStr = printableFlipResult(tossResult)
-      printGame(printableFlipResultStr, state)
-      gameLoop(r, newState)
-    }
-  }
-
-  gameLoop(r, state)
-  //  while (input != "q"){
+  //  @tailrec
+  //  def gameLoop(r: Random, state: GameState): Unit = {
   //    showPrompt
   //    input = getUserInput
-  //    if (input == "q"){
+  //    if (input == "q") {
   //      printGameOver()
   //    }
-  //    else{
+  //    else {
   //      val tossResult = tossCoin(r)
-  //      if(tossResult == input) {
-  //        state = state.copy(flipsCount = state.flipsCount + 1, correctGuesses = state.correctGuesses + 1)
+  //      val newState = if (tossResult == input) {
+  //        state.copy(flipsCount = state.flipsCount + 1, correctGuesses = state.correctGuesses + 1)
   //      }
-  //      else{
-  //        state = state.copy(flipsCount = state.flipsCount + 1)
+  //      else {
+  //        state.copy(flipsCount = state.flipsCount + 1)
   //      }
   //      val printableFlipResultStr = printableFlipResult(tossResult)
   //      printGame(printableFlipResultStr, state)
+  //      gameLoop(r, newState)
   //    }
+  //  }
+  //
+  //  gameLoop(r, state)
 
+  override def run(args: List[String]): IO[ExitCode] = {
+    def mainLoop(state: GameState): IO[Unit] =
+      for {
+        _ <- GameIO.showPrompt
+        input <- GameIO.getUserInput
+        tossResult <- GameIO.tossCoin(r)
+        printableFlipResult <- GameIO.printableFlipResult(tossResult)
+        new_state <- GameIO.calcualteState(input, tossResult, state)
+        _ <- GameIO.printGame(printableFlipResult, new_state)
+        _ <- input match {
+          case "q" => IO.unit
+          case _ => mainLoop(new_state)
+        }
+      } yield ()
+
+    for {
+      //      console <- IO.pure(Console[IO])
+      _ <- mainLoop(state)
+    } yield ExitCode.Success
+  }
 }
