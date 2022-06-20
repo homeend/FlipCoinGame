fe5d6420fd62c065286f6a18e3b9ab18207b6d64
Micha Rutkowski <mrutkowski@globeandmail.com>
2022-06-20 10:16:58 +0200

    wip 2022/06/20 10:16:58

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,15 +1,12 @@
 package example
 
-import cats.{Comparison, Monad}
 import cats.effect.{ExitCode, IO, IOApp, Sync}
 import cats.syntax.all._
-import example.Game._
 import example.Side.{Heads, Tails}
-import org.http4s.server.middleware.HttpMethodOverrider.HeaderOverrideStrategy
 
-import scala.annotation.tailrec
 import scala.io.StdIn
 import scala.util.Random
+import scala.util.control.NoStackTrace
 
 case class GameState(flipsCount: Int, correctGuesses: Int)
 
@@ -52,49 +49,55 @@ object Side {
   final case object Tails extends Side
 }
 
-sealed abstract class Command(val value: String)
-sealed abstract class EmptyCommand extends Command("")
-sealed abstract class Side2 extends EmptyCommand
+sealed trait Command
 
-final object Heads2 extends Side2
-final object Tails2 extends Side2
-final object Quit extends EmptyCommand
-final case class Unknown(str: String) extends Command(str)
+sealed trait Side2 extends Command
+
+object Side2 {
+  final object Heads2 extends Side2
+
+  final object Tails2 extends Side2
+}
+
+final object Quit extends Command
 
 
-//object Command {
-//  final case object Quit extends EmptyCommand
-//  final case class Unknown(str: String) extends EmptyCommand
-//}
+sealed trait ValidationError extends Throwable with NoStackTrace {
+  def getValue() = ""
+}
 
-//object Side2 {
-//  final case object Heads extends EmptyCommand
-//  final case object Tails extends EmptyCommand
-//}
+object ValidationError {
+
+  final case class UnknownCommand(str: String) extends ValidationError {
+    override def getMessage: String = s"Unknown input: $str"
+
+    override def getValue(): String = str
+  }
+}
+
 
 object GameIO {
   def showPrompt = IO.delay(print("\n(h)eads, (t)ails, or (q)uit: "))
 
   def getUserInput = IO.delay(StdIn.readLine().trim.toLowerCase)
 
-  def parseUserInput(input: String): IO[Command] = input match {
-    case "h" => IO.pure(Heads2)
-    case "t" => IO.pure(Tails2)
-    case "q" => IO.pure(Quit)
-    case _ => IO.pure(Unknown(input))
+
+  def parseUserInput(input: String): IO[Either[ValidationError, Command]] = input match {
+    case "h" => Right(Side2.Heads2).pure[IO]
+    case "t" => Right(Side2.Tails2).pure[IO]
+    case "q" => Right(Quit).pure[IO]
+    case _ => Left(ValidationError.UnknownCommand(input)).pure[IO]
   }
 
-  def parseUserInput2(input: String): Command = input match {
-    case "h" => Heads2
-    case "t" => Tails2
-    case "q" => Quit
-    case _ => Unknown(input)
-  }
-
-  def printableFlipResult(flip: Side) = IO.pure(flip match {
+  def printableFlipResult_(flip: Side) = IO.pure(flip match {
     case Heads => "Heads"
     case Tails => "Tails"
   })
+
+  def printableFlipResult(flip: Side) = flip match {
+    case Heads => "Heads".pure[IO]
+    case Tails => "Tails".pure[IO]
+  }
 
   def formatFlipResult(flip: Side) = flip match {
     case Heads => "Heads"
@@ -102,8 +105,8 @@ object GameIO {
   }
 
   def formatFlipResult2(flip: Side2) = flip match {
-    case Heads2 => "Heads"
-    case Tails2 => "Tails"
+    case Side2.Heads2 => "Heads"
+    case Side2.Tails2 => "Tails"
   }
 
   def printGame(flipResult: Side, state: GameState) = {
@@ -131,19 +134,20 @@ object GameIO {
     else {
       state.copy(flipsCount = state.flipsCount + 1)
     }
-    IO.pure(newState)
+    newState.pure[IO]
   }
 
   def tossCoin(r: Random) = {
-    IO.pure(r.nextInt(2) match {
+    IO.delay(r.nextInt(2) match {
       case 0 => Heads
       case 1 => Tails
     })
   }
+
   def tossCoin2(r: Random) = {
-    IO.pure(r.nextInt(2) match {
-      case 0 => Heads2
-      case 1 => Tails2
+    IO.delay(r.nextInt(2) match {
+      case 0 => Side2.Heads2
+      case 1 => Side2.Tails2
     })
   }
 }
@@ -184,57 +188,27 @@ object GameApp extends IOApp {
   val state: GameState = GameState(0, 0)
   val r = new Random()
 
-  //  @tailrec
-  //  def gameLoop(r: Random, state: GameState): Unit = {
-  //    showPrompt
-  //    input = getUserInput
-  //    if (input == "q") {
-  //      printGameOver()
-  //    }
-  //    else {
-  //      val tossResult = tossCoin(r)
-  //      val newState = if (tossResult == input) {
-  //        state.copy(flipsCount = state.flipsCount + 1, correctGuesses = state.correctGuesses + 1)
-  //      }
-  //      else {
-  //        state.copy(flipsCount = state.flipsCount + 1)
-  //      }
-  //      val printableFlipResultStr = printableFlipResult(tossResult)
-  //      printGame(printableFlipResultStr, state)
-  //      gameLoop(r, newState)
-  //    }
-  //  }
-  //
-  //  gameLoop(r, state)
+  def handleError(error: ValidationError): IO[Unit] = IO.delay(println(s"Error: ${error.getValue()}"))
 
   override def run(args: List[String]): IO[ExitCode] = {
     def mainLoop(state: GameState): IO[Unit] =
       for {
         _ <- GameIO.showPrompt
         input <- GameIO.getUserInput
-//        token <- GameIO.parseUserInput(input)
-//        token <- GameIO.parseUserInput(input).handleErrorWith(_ => IO.delay(println(s"unknown input: ${input}")).*>(IO.none))
-        token = GameIO.parseUserInput2(input)
-        new_state <- token match {
-          case flip: Side2 =>
-            for {
-              tossResult <- GameIO.tossCoin2(r)
-              new_state <- GameIO.calcualteState(tossResult, flip, state)
-              _ <- GameIO.printGame2(flip, new_state)
-              _ <- mainLoop(new_state)
-            } yield ()
-
-          case Quit => IO.pure(state)
-          case Unknown(str) => IO.delay(println(s"unknown input: ${str}")).*>(mainLoop(state))
+        token <- GameIO.parseUserInput(input)
+        _ <- token.fold(handleError(_).*>(mainLoop(state)), cmd => cmd match {
+          case flip: Side2 => for {
+            tossResult <- GameIO.tossCoin2(r)
+            new_state <- GameIO.calcualteState(tossResult, flip, state)
+            _ <- GameIO.printGame2(flip, new_state)
+            _ <- mainLoop(new_state)
+          } yield IO.unit
+          case Quit => IO.unit
         }
-//        _ <- token match {
-//          case Quit => IO.unit
-//          case _ => mainLoop(new_state)
-//        }
+        )
       } yield ()
 
     for {
-//      _ <- mainLoop(state).handleErrorWith(_ => IO.delay(println(s"unknown input: ${input}")))
       _ <- mainLoop(state)
     } yield ExitCode.Success
   }
